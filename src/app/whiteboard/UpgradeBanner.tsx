58f2a962f3393edfc8e84537acf03d11398508a7
Jakov C <qwerasdftzu@gmail.com>
2025-06-18 14:00:23 +0200

    Fix build error

--- src/app/whiteboard/UpgradeBanner.tsx
+++ src/app/whiteboard/UpgradeBanner.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { X, Check, Sparkles, ArrowRight, Crown, Zap } from "lucide-react";
 import { useQuery } from "convex/react";
-import { api } from "convex/_generated/api";
+import { api } from "../../../convex/_generated/api";
 
 const plans = [
   {
