13ecd48f8d2e4d25e291a3d7eaf8b50570bf1991
Jakov C <qwerasdftzu@gmail.com>
2025-07-08 18:58:16 +0200

    Add vercel speed insights

--- src/app/_components/providers/index.tsx
+++ src/app/_components/providers/index.tsx
@@ -1,14 +1,18 @@
 import type { ReactNode } from "react";
 import AuthProvider from "./AuthProvider";
 import { ConvexQueryCacheProvider } from "convex-helpers/react/cache/provider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 type Props = {
   children: ReactNode;
 };
 export default function Providers({ children }: Props) {
   return (
-    <AuthProvider>
-      <ConvexQueryCacheProvider>{children}</ConvexQueryCacheProvider>
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <ConvexQueryCacheProvider>{children}</ConvexQueryCacheProvider>
+      </AuthProvider>
+      <SpeedInsights />
+    </>
   );
 }
