449e963f83a523dd9828edbeb95f6ee0f1a0c8f7
Jakov C <qwerasdftzu@gmail.com>
2025-07-15 15:23:46 +0200

    Add google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,9 @@ export const metadata: Metadata = {
   description: "TODO: Add a description for AI Flow Studio",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
   keywords: ["AI Flow Studio", "Whiteboard AI", "Nodes", "Flow"],
+  verification: {
+    google: "p4gzWQ-1iXVG0l_lfeyeSHMr_37F_pq6QH3hu8zmL40",
+  },
 };
 
 const nunito = Nunito({
