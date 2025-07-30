f862694ae9c3634cbde83240196d8b28a28c6d8e
Jakov C <qwerasdftzu@gmail.com>
2025-07-30 19:02:16 +0200

    Add 3 dots when nodes are executing

--- src/app/whiteboard/ProgressBar.tsx
+++ src/app/whiteboard/ProgressBar.tsx
@@ -24,9 +24,32 @@ export default function ProgressBar() {
           className="bg-muted fixed top-16 left-1/2 z-[99999999] w-[100px] max-w-md -translate-x-1/2 rounded-md px-4 py-2 shadow backdrop-blur sm:w-[100px] md:w-[150px] lg:w-[200px]"
         >
           <div className="text-muted-foreground mb-1 text-center text-xs">
-            Executing Flow... ({executedNodesCount}/{totalNodesForExecution})
+            Executing Flow<span className="dots-animation"></span>
           </div>
+
           <Progress value={progress} className="h-1" />
+
+          {/* Pure CSS animation */}
+          <style jsx>{`
+            .dots-animation::after {
+              margin-left: 1px;
+              content: "";
+              display: inline-block;
+              animation: dotCycle 2s steps(1, end) infinite;
+            }
+
+            @keyframes dotCycle {
+              0% {
+                content: ".";
+              }
+              33% {
+                content: "..";
+              }
+              66% {
+                content: "...";
+              }
+            }
+          `}</style>
         </motion.div>
       )}
     </AnimatePresence>
